bd51e39bbdea6f65ca36c3e7ce49204beb03df53
GoD139 <bb-business@hotmail.com>
2018-08-06 12:40:18 +0200

    Add files via upload

--- fanpoints/class/controller.class.php
+++ fanpoints/class/controller.class.php
@@ -8,13 +8,24 @@
 class controller extends fanPoints
 {
 
+
+
   function __construct()
   {
+
+
+
+    // echo '<pre>';
+    // print_r($this->Woocommerce);
+    // echo '</pre>';
+
     // add the save action to user's own profile editing screen update
     add_action( 'personal_options_update' , array( $this , 'usermeta_form_field_fanpoint_update' ) );
 
     // add the save action to user profile editing screen update
     add_action( 'edit_user_profile_update' , array( $this , 'usermeta_form_field_fanpoint_update' ) );
+
+    add_action( 'woocommerce_thankyou', array( $this, 'add_points_after_purchase'));
 
     if(isset($_POST['submit'])){
       $this->options_update();
@@ -37,8 +48,7 @@ class controller extends fanPoints
       }
 
       // create/update user meta for the $user_id
-      return update_user_meta( $user_id, 'FanPoints', $_POST['fanpoints']
-      );
+      return update_user_meta( $user_id, 'FanPoints', $_POST['fanpoints']);
   }
 
 
@@ -50,12 +60,16 @@ class controller extends fanPoints
   */
   function options_update()
   {
-    $Worth = $_POST['FP_Worth'];
 
-    if(get_option('fanpoint_options', false)){
-      add_option( 'FanPoints_Worth', $Worth );
+    $Settings = array(
+      'FP_Worth' => $_POST['FP_Worth'],
+      'FP_Receives_Basic' => $_POST['FP_Receives_Basic'],
+      'FP_Receives_Premium' => $_POST['FP_Receives_Premium']);
+
+    if(!get_option('fanpoint_options', false)){
+      add_option( 'fanpoint_options', $Settings );
     }else{
-      update_option( 'FanPoints_Worth', $Worth );
+      update_option( 'fanpoint_options', $Settings );
     }
 
   }
@@ -63,6 +77,72 @@ class controller extends fanPoints
 
 
 
+
+
+
+ function add_points_after_purchase()
+ {
+    if($this->is_user_logged_in()){
+
+
+      echo 'is logged in';
+
+
+      echo $this->get_order_price();
+
+      $this->give_points($this->get_order_price());
+
+
+
+    }else{
+      echo 'is NOT logged in';
+    }
+ }
+
+
+ //check if user have already received points for their order
+ //so they dont get more points if they reload their thank-you page
+ function check_if_order_have_given_points()
+ {
+
+ }
+
+
+
+
+
+  private function give_points($purchaseCost)
+  {
+    if (wc_memberships_is_user_active_member($user_id, 'Premium Subscription')){
+      $amount = get_option('fanpoint_options')['FP_Receives_Premium'] * $purchaseCost;
+    }else if (wc_memberships_is_user_active_member($user_id, 'Basic Subscription')){
+      $amount = get_option('fanpoint_options')['FP_Receives_Basic'] * $purchaseCost;
+    }
+    return update_user_meta( get_current_user_id(), 'FanPoints', $amount);
+  }
+
+
+ //MISC
+
+ private function is_user_logged_in()
+ {
+   $user = wp_get_current_user();
+   return $user->exists();
+ }
+
+
+ private function get_order_price()
+ {
+   $orders = new WP_Query($this->GetOrdersFilter);
+
+   foreach($orders->posts as $order){
+     if($orders->post->ID == $order->ID){
+       $orderPrice = get_post_meta($order->ID, '_order_total')[0];
+     }
+   }
+
+   return $orderPrice;
+ }
 
 
 
--- fanpoints/class/display_fields.class.php
+++ fanpoints/class/display_fields.class.php
@@ -18,11 +18,12 @@ class displayFields extends fanPoints
     // add the field to user profile editing screen
     add_action( 'show_user_profile', array($this, 'fanpoints_usermeta_form_field_admin') );
 
-    // add the custom userlist column
-    add_filter( 'manage_users_columns', array($this, 'new_modify_user_table') );
 
-    // add the custom userlist column data
-    add_filter( 'manage_users_custom_column', array($this, 'new_modify_user_table_row'), 10, 3  );
+    add_filter( 'manage_users_columns', array($this, 'fanpoints_user_table') );// add the custom userlist column
+    add_filter( 'manage_users_custom_column', array($this, 'fanpoints_user_table_row'), 10, 3  ); // add the custom userlist column data
+
+
+    // add_filter( 'manage_users_custom_column', array($this, 'fanpoints_to_dkk_user_table_row'), 11, 4  );// add the custom userlist column data
 
     // add custom FanPoint menu
     add_action( 'admin_menu', array( $this ,'settings_menu' ) );
@@ -68,25 +69,35 @@ class displayFields extends fanPoints
    *
    * @param $column userlist var
    */
-  function new_modify_user_table( $column ) {
+  function fanpoints_user_table( $column ) {
       $column['FanPoints'] = 'FanPoints';
       return $column;
   }
 
 
+
   /**
    * Add data to user list column
    */
-  function new_modify_user_table_row( $val, $column_name, $user_id ) {
+  function fanpoints_user_table_row( $val, $column_name, $user_id ) {
+
+
+    $UsersFanPoints = get_the_author_meta( 'FanPoints', $user_id );
+
     if($column_name == 'FanPoints'){
-      return get_the_author_meta( 'FanPoints', $user_id );
+      if($UsersFanPoints){
+        return $UsersFanPoints .' Points <br>'. $UsersFanPoints * floatval(get_option('fanpoint_options', false)['FP_Worth']) . '/dkk';
+      }
+      return $UsersFanPoints;
     }
     return $val;
   }
 
 
-
-
+  private function FP_Worth()
+  {
+    return (float)$this->Settings['FP_Worth'];
+  }
 
 
 
@@ -98,17 +109,47 @@ class displayFields extends fanPoints
 
     <form method="post" action="'. admin_url( 'users.php?page=fanpoints' ) .'">
 
-      <label>1 point is worth</label><br>
-      <input type="number"
-             class="regular-text ltr"
-             id="fanpoints"
-             name="FP_Worth"
-             value="'. esc_attr(get_option('FanPoints_Worth')) .'"
-             title="FanPoints worth"
-             pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])"
-             step="0.0001"
-             required>
-       <label>DKK.</label>
+      <div class="form-group">
+        <label>1 point is worth</label><br>
+        <input type="number"
+               class="regular-text ltr"
+               id="fanpoints"
+               name="FP_Worth"
+               value="'. esc_attr(get_option('fanpoint_options')['FP_Worth']) .'"
+               title="FanPoints worth"
+               pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])"
+               step="0.0001"
+               required>
+         <label>DKK.</label>
+       </div>
+       <h2>Subscription Settings</h2>
+       <div class="form-group">
+         <label>Basic Subscription gets </label><br>
+         <input type="number"
+                class="regular-text ltr"
+                id="fanpoints"
+                name="FP_Receives_Basic"
+                value="'. esc_attr(get_option('fanpoint_options')['FP_Receives_Basic']) .'"
+                title="FanPoints worth"
+                pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])"
+                step="0.0001"
+                required>
+          <label>points per 1 DKK spent</label>
+        </div>
+
+        <div class="form-group">
+          <label>Premium Subscription gets </label><br>
+          <input type="number"
+                 class="regular-text ltr"
+                 id="fanpoints"
+                 name="FP_Receives_Premium"
+                 value="'. esc_attr(get_option('fanpoint_options')['FP_Receives_Premium']) .'"
+                 title="FanPoints worth"
+                 pattern="(19[0-9][0-9]|20[0-9][0-9])-(1[0-2]|0[1-9])-(3[01]|[21][0-9]|0[1-9])"
+                 step="0.0001"
+                 required>
+           <label>points per 1 DKK spent</label>
+         </div>
 
 
 
