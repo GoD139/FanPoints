0f6d82eb7fa3ed46c50f24e7c85096522e3da192
GoD139 <bb-business@hotmail.com>
2018-08-16 15:01:59 +0200

    Add files via upload

---
+++ class/purchase.class.php
@@ -0,0 +1,147 @@
+<?php
+
+
+
+
+
+class purchase extends fanPoints
+{
+
+  private $apply_btn_title = 'Brug Rabat';
+  private $remove_btn_title = 'Fjern Rabat';
+
+  function __construct()
+  {
+    //show message to add or remove rewards
+    add_action( 'woocommerce_before_cart', array($this, 'phoen_rewpts_action_woocommerce_before_cal_table'), 10, 0);
+
+    $this->fp_add_fee_from_cart();
+    $this->fp_remove_fee_from_cart();
+
+  }
+
+
+  function phoen_rewpts_action_woocommerce_before_cal_table() {
+
+    global $woocommerce;
+
+    //$curr=get_woocommerce_currency_symbol();
+
+    //$this->getCurrentUsersPoints();
+
+
+    $bill_price=$woocommerce->cart->cart_contents_total;
+
+    $taxPrice = preg_replace('/[^0-9]/', '', $woocommerce->cart->tax_total);
+
+    //$used_reward_amount = $woocommerce->cart->fee_total;
+
+    //echo $this->getCurrentUsersPoints();
+    if(round($this->getCurrentUsersPoints()) != 0)
+    {
+      ?>
+      <div class="col-md-12 clearfix" style="background:#fff; padding:3px 2px; margin:50px 10px;">
+        <div class="fanpoint_apply_points_text col-md-9 float-left">
+
+      <?php
+
+      echo '<img src="'. esc_url( plugins_url( 'assets/img/coins.svg', __DIR__ ) ) .'" class="float-left" height="50px" style="margin-right:10px;">';
+
+      if($bill_price >= $this->getPointsWorth($this->getCurrentUsersPoints()))
+      {
+        if(!isset($_SESSION['fp_action']) || $_SESSION['fp_action'] == "remove")
+        {
+          echo '
+          <p class="fp_discount_text fp_not_active">
+          Du kan bruge dine <span class="fp_point_color" style="color:#50ad90;">'. $this->getCurrentUsersPoints() . '</span> FanPoints og få '. round($this->getPointsWorth($this->getCurrentUsersPoints()), 1)  .' DKK rabat!</p>';
+        }else if($_SESSION['fp_action'] == "apply"){
+          echo '<p class="fp_discount_text fp_active">Rabat tilføjet!</p>';
+        }
+
+      }
+
+      ?>
+
+      </div>
+
+      <div class="fanpoint_apply_points_form col-md-3 float-right">
+
+        <form method="post" action="">
+
+          <?php
+
+          if(!isset($_SESSION['fp_action']) || $_SESSION['fp_action'] == "remove")
+          {
+            echo '<input type="submit" class="btn btn-apply-points col-md-12"  value="'. $this->apply_btn_title .'" name="fp_apply_points">';
+          }else if($_SESSION['fp_action'] == "apply"){
+            echo '<input type="submit" class="btn btn-remove-points col-md-12"  value="'. $this->remove_btn_title .' " name="fp_remove_points">';
+          }
+
+          ?>
+
+
+
+
+        </form>
+
+      </div>
+     </div>
+      <?php
+    }
+  }
+
+
+
+
+
+
+
+
+
+  //remove reward points from total if click on rmove points
+  function fp_remove_fee_from_cart()
+  {
+    if(isset($_POST['fp_remove_points'])) {
+      remove_action( 'woocommerce_cart_calculate_fees', array( $this , 'fp_woo_add_cart_fee' ),10,1);
+      $_SESSION['fp_action']="remove";
+    }
+  }
+
+  //add reward points to total if click on rmove points
+  function fp_add_fee_from_cart()
+  {
+    if(isset($_POST['fp_apply_points'])) 	{
+      add_action( 'woocommerce_cart_calculate_fees', array( $this , 'fp_woo_add_cart_fee' ), 10, 1);
+      $_SESSION['fp_action']="apply";
+    }
+  }
+
+
+
+
+
+  function fp_woo_add_cart_fee() {
+
+    global $woocommerce;
+
+    $amt = round($this->getPointsWorth($this->getCurrentUsersPoints()), 1);
+    $bill_price = $woocommerce->cart->cart_contents_total;
+
+    $u_price=0;
+
+    if($amt >= $bill_price) {
+      $u_price = $bill_price;
+    } else if($amt<$bill_price) {
+      $u_price = $amt;
+    }
+
+      $woocommerce->cart->add_fee( __('FanPoint Rabat', 'woocommerce'), "-".$u_price );
+  }
+
+
+
+
+
+
+
+}
--- fanpoints.php
+++ fanpoints.php
@@ -20,24 +20,23 @@
    include_once('class/display_fields.class.php');
    include_once('class/controller.class.php');
    include_once('class/recurring.class.php');
+   include_once('class/purchase.class.php');
 
     //wc_get_order_id_by_order_key($_GET['key'])
 
     //print_r(wc());
 
    $fanPoints = new fanPoints();
-   $display = new displayFields();
-   $controller = new controller();
-   $recurring = new recurring();
-
+   $fp_display = new displayFields();
+   $fp_controller = new controller();
+   $fp_recurring = new recurring();
+   $fp_purchase = new purchase();
 
 
    class fanPoints
    {
 
      protected $Title = 'FanPoints';
-
-     protected $Settings;
 
      protected $GetOrdersFilter = array(
      'post_status' => 'any',
@@ -49,10 +48,56 @@
 
      function __construct()
      {
-
-       $this->Settings = get_option('fanpoint_options', false)['FP_Worth'];
-
+       add_action( 'wp_enqueue_scripts', array($this , 'fanpoints_style') );
      }
+
+
+
+     function fanpoints_style()
+     {
+         wp_register_style( 'fp-style', plugins_url( '/assets/css/fp-style.css', __FILE__ ), array(), '20120209', 'all' );
+
+         // For either a plugin or a theme, you can then enqueue the style:
+         wp_enqueue_style( 'fp-style' );
+     }
+
+
+
+
+     //fetch currently logged in users points
+     protected function getCurrentUsersPoints()
+     {
+       return get_user_meta(get_current_user_id() , 'FanPoints')[0];
+     }
+
+     //fetch the worth of a certain amount of points
+     protected function getPointsWorth($points = 1)
+     {
+       return $points * floatval(get_option('fanpoint_options', false)['FP_Worth']);
+     }
+
+     protected function numberFormatter($n) {
+        // first strip any formatting;
+        $n = (0+str_replace(",","",$n));
+
+        // is this a number?
+        if(!is_numeric($n)) return false;
+
+        // now filter it;
+        if($n>1000000000000) return round(($n/1000000000000),1).' trillion';
+        else if($n>1000000000) return round(($n/1000000000),1).' milliarder';
+        else if($n>1000000) return round(($n/1000000),1).' millioner';
+        else if($n>1000) return round(($n/1000),1).' tusind';
+
+        return number_format($n);
+    }
+
+
+     protected function cartPrice()
+     {
+       return $woocommerce->cart->cart_contents_total;
+     }
+
    }
 
 
