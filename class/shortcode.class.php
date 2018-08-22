84e643e6d1a33c1cc846aae9b74f5dea36b18ef8
GoD139 <bb-business@hotmail.com>
2018-08-22 17:22:15 +0200

    Add files via upload

--- class/shortcode.class.php
+++ class/shortcode.class.php
@@ -12,17 +12,18 @@ class shortcodes extends fanPoints
   function __construct()
   {
     add_shortcode( 'users_fanpoints', array($this, 'fp_display_users_fanpoints') );
+    add_shortcode( 'users_fanpoints_worth', array($this, 'fp_display_users_fanpoints_worth') );
   }
   
   
   
   function fp_display_users_fanpoints( $atts ) {
-  
       return $this->getCurrentUsersPoints();
-  
   }
   
-  
+  function fp_display_users_fanpoints_worth( $atts ) {
+      return $this->getPointsWorth($this->getCurrentUsersPoints());
+  }
   
   
 }
