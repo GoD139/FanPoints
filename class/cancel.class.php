631e8d8f52188d12c7967ff31ee5b6d8729b2900
flyberson <flyberson12@gmail.com>
2018-10-19 10:07:29 +0200

    Add files via upload

--- class/cancel.class.php
+++ class/cancel.class.php
@@ -9,12 +9,12 @@ class cancel extends fanPoints {
 
 	function __construct() {
 		add_action("init",array( $this , 'init' ));
-		//$this->remove_fanpoints_woocommerce_cancelled_order("4567");
 	}
 
 function init(){
-	add_action( 'woocommerce_order_status_cancelled', 'remove_fanpoints_woocommerce_cancelled_order', 10, 1 );
-}
+	add_action( 'woocommerce_order_status_cancelled', array( $this , 'remove_fanpoints_woocommerce_cancelled_order' ), 21, 1 );
+
+	}
 
 	function remove_points() {
 
@@ -30,12 +30,21 @@ function init(){
 	}
 
 	function remove_fanpoints_woocommerce_cancelled_order($order_get_id){
-		//echo "hello gets run";
-		//echo did_action('init');
 		$order = wc_get_order( $order_get_id );
 		$orderTotal=$order->get_total();
-		$orderFanpointValue=$this->get_fanpoint_value_basic_product($orderTotal);
+
+
 		$userid=$order->get_user_id();
+		if($this->isBasicMemberid($userid)) {
+			$orderFanpointValue = $this->get_fanpoint_value_basic_product( $orderTotal );
+		}
+
+		elseif ($this->isPremiumMemberid($userid)){
+			$orderFanpointValue = $this->get_fanpoint_value_premium_product( $orderTotal );
+
+		}else{
+			$orderFanpointValue=0;
+		}
 		if(get_user_meta($userid, 'FanPoints')[0]){
 			$userFanpointAmount= get_user_meta($userid , 'FanPoints')[0];
 		}
@@ -48,6 +57,10 @@ function init(){
 		update_user_meta( $userid, 'FanPoints', $userFanpointAmount);
 
 	}
+
+
+
+
 
 
 }
