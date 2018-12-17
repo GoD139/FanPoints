e55c495dde264cc12a0bc2ad7139094f15641d4e
GoD139 <bb-business@hotmail.com>
2018-12-17 10:20:39 +0100

    Add files via upload

--- class/recurring.class.php
+++ class/recurring.class.php
@@ -1,165 +1,79 @@
-<?php 
-
-
-
-
-/**
- * 
- */
-class FB_Recurring extends Fanbank
+<?php
+class recurring extends fanPoints
 {
-  
   private $oneMonthFromNow;
-  private $userPaysMonthly = 139;
-  private $userIsPaidMonthly = 550;
-  
   function __construct()
   {
     $this->oneMonthFromNow = date("Y-m-d H:i:s", strtotime("+1 month"));
-
-    add_action('user_register', array($this, 'executeOnUserRegistration'));
+    // add_action('user_register', array($this, 'executeOnUserRegistration'));
     
     //check if a user should get their recurring monthly bonus
-    add_action( 'init', array($this, 'RecurrenceChecker') );
+    // add_action( 'init', array($this, 'RecurrenceChecker') );
   }
-  
   function executeOnUserRegistration($user_id){
     $this->createDateToRecurre($userID);
   }
   
   public function RecurrenceChecker()
   {
-    
-    // echo '<pre>';
-    // //print_r(get_users());
-    // echo '</pre>';
     foreach(get_users() as $Users){
-      
-      //echo 'get users ';
-      //echo $Users->ID.'<br>';
-      if($this->checkIfFanBankMember($Users->ID)){
-        //echo 'get premium <br>';
-      
+      //echo 'get users <br>';
+      if($this->checkIfPremium($Users->ID)){
+        //echo 'is premium <br>';
         //echo get_user_meta($Users->ID, 'FP_Recurring')[0];
-        if($this->checkifMoreThanAMonthAgo(get_user_meta($Users->ID, 'FB_Recurring')[0])){
+        if($this->checkifMoreThanAMonthAgo(get_user_meta($Users->ID, 'FP_Recurring')[0])){
           //echo 'Is more than a month ago <br>';
-      
-      
-          $this->addToUserPayRecord($Users->ID);//what the user pays every month of their subscription towards paying their "debt"
-          
-          if(!$this->UserPaidEnough($Users->ID)){
-            $this->addToUserRecievedRecord($Users->ID);//add to record of how much user have received
-            
-            if($this->getUsersSubscribed() < 10){
-              $this->givePoints($Users->ID, 3300);//give points
-              echo 'gives 3300';
-            }else{
-              $this->givePoints($Users->ID, 550);//give points
-              echo 'gives 550';
-            }
-          }
+          $this->givePoints($Users->ID, 7800);//give points
           $this->createDateToRecurre($Users->ID);//give new recurring date
         }
       }
     }
   }
-
-
-
   public function createDateToRecurre($userID)
   {
-    update_user_meta( $userID, 'FB_Recurring', $this->oneMonthFromNow );
+    update_user_meta( $userID, 'FP_Recurring', $this->oneMonthFromNow );
   }
-
-
-
-  private function addToUserPayRecord($userID)
+  private function checkIfPremium($userID)
   {
-    $record = array();
-    $oldRecord = get_user_meta($userID , 'FanBank_record')[0];
-    $oldRecord_reformated = json_decode($oldRecord);
-    
-
-    //print_r($oldRecord_reformated);
-    
-    if(isset($oldRecord_reformated)){
-      foreach($oldRecord_reformated as $rec)
-      {
-        array_push($record, array( "Date" => $rec->Date, "Paid" => $rec->Paid ));
-      }
-    }
-    
-    array_push($record, array( 'Date' => date("Y-m-d H:i:s"), 'Paid' => $this->userPaysMonthly ));
-    $record = json_encode($record);
-    
-    return update_user_meta( $userID, 'FanBank_record', $record);
-  } 
-  
-  
-  private function addToUserRecievedRecord($userID)
-  {
-    $record = array();
-    $oldRecord = get_user_meta($userID , 'FanBank_received')[0];
-    $oldRecord_reformated = json_decode($oldRecord);
-    
-    $paid = $this->userIsPaidMonthly;
-    if($this->getUsersSubscribed() < 10){
-      $paid = 3300;
-    }
-    
-    print_r($oldRecord_reformated);
-    
-    if(isset($oldRecord_reformated)){
-      foreach($oldRecord_reformated as $rec)
-      {
-        array_push($record, array( "Date" => $rec->Date, "Paid" => $rec->Paid ));
-      }
-    }
-    
-    array_push($record, array( 'Date' => date("Y-m-d H:i:s"), 'Paid' => $paid ));
-    $record = json_encode($record);
-    
-    return update_user_meta( $userID, 'FanBank_received', $record);
-  } 
-
-
-
-  private function checkIfPremiumWithAddon($userID)
-  {
-    if (wc_memberships_is_user_active_member($userID, 'Premium Subscription') ){
+    if (wc_memberships_is_user_active_member($userID, 'Premium Subscription')){
       return true;
     }
     return false;
   }
-  
-  
-
-
-
   private function checkifMoreThanAMonthAgo($date)
   {
-    if ($date < date("Y-m-d H:i:s")){
+    if ($date < date("Y-m-d H:i:s"))
+    {
       return true;
     }
     return false;
+    // 
+    // if(strtotime($date) > strtotime('+2 min')) {
+    //   return true;
+    // }
+    // return false;
   }
-
-
-
-
+  
+  
   private function givePoints($userID, $points)
   {
-    $FanBank_Count = 0;
-    if(isset(get_user_meta($userID , 'FanBank')[0])){
-      $FanBank_Count = get_user_meta($userID , 'FanBank')[0];
+    $PlayersCurrentPoints = 0;
+    $FanPoints = get_user_meta($userID , 'FanPoints')[0];
+    if (wc_memberships_is_user_active_member($userID, 'Premium Subscription')){
+      $amount = $points;
+      $amount += $FanPoints;
     }
-    $recieves = $FanBank_Count + $points;
-    return update_user_meta( $userID, 'FanBank', $recieves);
+    return update_user_meta( $userID, 'FanPoints', $amount);
   }
-
-
-
-
-
-
+  
+  /////////////// testing ////////////////
+  public function giveEveryoneRecurringDate()
+  {
+    foreach(get_users() as $Users){
+        $this->createDateToRecurre($Users->ID);
+        //if(get_user_meta($Users->ID, 'FP_Recurring'))
+        // echo get_user_meta($Users->ID, 'FP_Recurring');
+        // echo $Users->ID;
+    }
+  }
 }
